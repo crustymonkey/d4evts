aa316aaa5a7a28c372488220acd87b2c024cb30c
Jay Deiman <jay@splitstreams.com>
2024-10-09 09:21:39 -0700

    Adding Realm Walker support

--- src/d4evts.rs
+++ src/d4evts.rs
@@ -31,15 +31,21 @@ struct Args {
 
 static LOGGER: GlobalLogger = GlobalLogger;
 // Setup the constants used for calculations
+// World Boss
 const WB_INIT: u64 = 1708381800;
 const WB_EVERY: u64 = 60 * 210; // Every 3.5 hours
+// Legion Event
 const LE_INIT: u64 = 1708381200;
 const LE_EVERY: u64 = 60 * 25; // Every 25 minutes
+// Realm Walker
+const RW_INIT: u64 = 1728414300;
+const RW_EVERY: u64 = 60 * 15; // Every 15 minutes
 
 /// This is used as an argument to calculate particular deltas
 enum EventType {
     WB,
     LE,
+    RW,
 }
 
 /// This is a struct that contains the core GUI app data
@@ -48,6 +54,8 @@ struct MyApp {
     wb_color: Arc<Mutex<Color32>>,
     le: Arc<Mutex<String>>,
     le_color: Arc<Mutex<Color32>>,
+    rw: Arc<Mutex<String>>,
+    rw_color: Arc<Mutex<Color32>>,
     font_size: f32,
 }
 
@@ -59,6 +67,8 @@ impl MyApp {
             wb_color: Arc::new(Mutex::new(Color32::GRAY)),
             le: Arc::new(Mutex::new("".to_string())),
             le_color: Arc::new(Mutex::new(Color32::GRAY)),
+            rw: Arc::new(Mutex::new("".to_string())),
+            rw_color: Arc::new(Mutex::new(Color32::GRAY)),
             font_size: font_size,
         };
     }
@@ -72,6 +82,8 @@ impl Default for MyApp {
             wb_color: Arc::new(Mutex::new(Color32::GRAY)),
             le: Arc::new(Mutex::new("".to_string())),
             le_color: Arc::new(Mutex::new(Color32::GRAY)),
+            rw: Arc::new(Mutex::new("".to_string())),
+            rw_color: Arc::new(Mutex::new(Color32::GRAY)),
             font_size: 30.0,
         };
     }
@@ -98,6 +110,14 @@ impl eframe::App for MyApp {
                         .size(self.font_size)
                         .color(Color32::BLACK)
                         .background_color(*self.le_color.lock().unwrap()),
+                );
+                ui.end_row();
+                ui.label(RichText::new("Realm Walker").size(self.font_size).strong());
+                ui.label(
+                    RichText::new(format!("{}", self.rw.lock().unwrap()))
+                        .size(self.font_size)
+                        .color(Color32::BLACK)
+                        .background_color(*self.rw_color.lock().unwrap()),
                 );
                 ui.end_row();
             });
@@ -171,7 +191,11 @@ fn calc_delta(ev: EventType, ts: Option<u64>) -> u64 {
         EventType::LE => {
             elapsed = (now - LE_INIT) % LE_EVERY;
             delta = LE_EVERY - elapsed;
-        }
+        },
+        EventType::RW => {
+            elapsed = (now - RW_INIT) % RW_EVERY;
+            delta = RW_EVERY - elapsed;
+        },
     };
 
     debug!("Got delta of: {delta}");
@@ -200,11 +224,14 @@ fn run_update_thread(
     wb_color: Arc<Mutex<Color32>>,
     le: Arc<Mutex<String>>,
     le_color: Arc<Mutex<Color32>>,
+    rw: Arc<Mutex<String>>,
+    rw_color: Arc<Mutex<Color32>>,
 ) {
     loop {
         thread::sleep(Duration::from_secs(1));
         let wb_delta = calc_delta(EventType::WB, None);
         let le_delta = calc_delta(EventType::LE, None);
+        let rw_delta = calc_delta(EventType::RW, None);
 
         *wb.lock().unwrap() = get_hms(wb_delta);
         if wb_delta <= 300 {
@@ -218,6 +245,12 @@ fn run_update_thread(
             *le_color.lock().unwrap() = Color32::LIGHT_RED;
         } else {
             *le_color.lock().unwrap() = Color32::GRAY;
+        }
+        *rw.lock().unwrap() = get_hms(rw_delta);
+        if rw_delta <= 300 {
+            *rw_color.lock().unwrap() = Color32::LIGHT_RED;
+        } else {
+            *rw_color.lock().unwrap() = Color32::GRAY;
         }
     }
 }
@@ -237,9 +270,11 @@ fn main() {
     let wb_color = app.wb_color.clone();
     let le = app.le.clone();
     let le_color = app.le_color.clone();
+    let rw = app.rw.clone();
+    let rw_color = app.rw_color.clone();
 
     thread::spawn(move || {
-        run_update_thread(wb, wb_color, le, le_color);
+        run_update_thread(wb, wb_color, le, le_color, rw, rw_color);
     });
 
     eframe::run_native(
@@ -281,6 +316,13 @@ mod tests {
         assert_eq!(
             LE_EVERY - (20 * 60 + 8),
             calc_delta(EventType::LE, Some(le_ts))
+        ); // 0:20:08
+
+        // init + 1:10:08
+        let rw_ts = RW_INIT + 3600 + 600 + 8;
+        assert_eq!(
+            RW_EVERY - (10 * 60 + 8),
+            calc_delta(EventType::RW, Some(rw_ts))
         ); // 0:20:08
     }
 }
